ae15572895ed3c580459776283382f8f9949cb0a
McKinney <brett-mckinney@Bretts-MacBook-Pro.local>
2018-08-13 21:41:22 -0500

    data.R

---
+++ R/data.R
@@ -0,0 +1,14 @@
+# stirdata.R - bam - 8/13/18
+
+#' RNA-Seq from the paper referenced below.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name mdd.RNASeq.0.8.filtered.corrected
+#' @usage data(mdd.RNASeq.0.8.filtered.corrected)
+#' @references
+#' Trang T. Le, et al. “Identification and replication of RNA-Seq gene network modules associated with depression severity,” Translational Psychiatry. 2018.
+#' @format
+#' Data frame with 157 mdd and hc subjects, 5912 genes.
+"mdd.RNASeq.0.8.filtered.corrected"
